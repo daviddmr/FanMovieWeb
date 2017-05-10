8e37674853de8378857f2758dd0d56cc7a47cb1b
David <david.dmr@gmail.com>
2017-05-09 22:41:13 -0300

    Ações de voltar a lista de filmes e realizar logout na tela de cadastro de filme

--- js/controllers/register-movie-controller.js
+++ js/controllers/register-movie-controller.js
@@ -2,8 +2,17 @@
  * Created by david on 09/05/2017.
  */
 angular.module("fanMovieWeb")
-    .controller('RegisterMovieController', function ($scope, registerMovieService) {
+    .controller('RegisterMovieController', function ($scope, $state, registerMovieService, movieListService) {
 
         $scope.title = "Cadastrar filme";
+
+        $scope.goToListOfMovie = function () {
+            $state.go("movie-list");
+        };
+
+        $scope.logout = function () {
+            movieListService.logout();
+            $state.go("login");
+        };
 
     });
