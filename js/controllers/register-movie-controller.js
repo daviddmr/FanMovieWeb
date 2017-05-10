a45ac5d60ac5165accd55c87d1725e1bd7aae2a7
David Melo <david.dmr@gmail.com>
2017-05-10 17:04:25 -0300

    Implementando spinner com opções de categorias de filme

--- js/controllers/register-movie-controller.js
+++ js/controllers/register-movie-controller.js
@@ -6,12 +6,28 @@ angular.module("fanMovieWeb")
 
         $scope.title = "Cadastrar filme";
         $scope.genres_selected = [];
+        $scope.currentGenre = {};
 
-        $scope.genres = ('Ação Aventura Animação Comédia Crime Documentário Drama Família Fantasia História ' +
-        'Terror Música Mistério Romance Ficção científica Cinema TV Thriller Guerra Faroeste')
-            .split(' ').map(function (genre) {
-                return {abbrev: genre};
-            });
+        $scope.genres = [
+            {"id": 28, genreName: 'Ação'},
+            {"id": 12, genreName: 'Aventura'},
+            {"id": 16, genreName: 'Animação'},
+            {"id": 35, genreName: 'Comédia'},
+            {"id": 80, genreName: 'Crime'},
+            {"id": 99, genreName: 'Documentário'},
+            {"id": 18, genreName: 'Drama'},
+            {"id": 10751, genreName: 'Família'},
+            {"id": 14, genreName: 'Fantasia'},
+            {"id": 36, genreName: 'História'},
+            {"id": 27, genreName: 'Terror'},
+            {"id": 10402, genreName: 'Música'},
+            {"id": 9648, genreName: 'Mistério'},
+            {"id": 10749, genreName: 'Romance'},
+            {"id": 878, genreName: 'Ficção científica'},
+            {"id": 10770, genreName: 'Cinema TV'},
+            {"id": 53, genreName: 'Thriller'},
+            {"id": 10752, genreName: 'Guerra'},
+            {"id": 37, genreName: 'Faroeste'}];
 
         $scope.goToListOfMovie = function () {
             $state.go("movie-list");
