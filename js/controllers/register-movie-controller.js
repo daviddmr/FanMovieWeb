a5cf47689b07de58c6d40ee83d7ccc1a2a745dfe
David <david.dmr@gmail.com>
2017-05-10 01:10:25 -0300

    Criação de alguns campos no formulário de cadastro de filme

--- js/controllers/register-movie-controller.js
+++ js/controllers/register-movie-controller.js
@@ -5,6 +5,13 @@ angular.module("fanMovieWeb")
     .controller('RegisterMovieController', function ($scope, $state, registerMovieService, movieListService) {
 
         $scope.title = "Cadastrar filme";
+        $scope.genres_selected = [];
+
+        $scope.genres = ('Ação Aventura Animação Comédia Crime Documentário Drama Família Fantasia História ' +
+        'Terror Música Mistério Romance Ficção científica Cinema TV Thriller Guerra Faroeste')
+            .split(' ').map(function (genre) {
+                return {abbrev: genre};
+            });
 
         $scope.goToListOfMovie = function () {
             $state.go("movie-list");
@@ -14,5 +21,9 @@ angular.module("fanMovieWeb")
             movieListService.logout();
             $state.go("login");
         };
+
+        $scope.addGenre = function (genre) {
+            $scope.genres_selected.push(genre)
+        }
 
     });
